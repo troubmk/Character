2f6d06ca72c52c52ccf4f58d86317396fa0d1874
Pham Thu <troubmk@gmail.com>
2026-07-14 06:51:51 +0700

    clothing

--- src/component/Partlist.js
+++ src/component/Partlist.js
@@ -5,7 +5,7 @@ function Partlist ({ title, total, path, handleClick}) {
   const icons = [];
    for (let i = 1; i <= total; i++) {
     icons.push((<Icon title={title} path={path} index={i} zoom={1} top="50%" handleClick={handleClick}/>))
-       }
+       } 
   return (  
           <div className="list">{icons}</div>
   )
